aa0a2ffd8afbc1cfca5d3a063b3667866f3680dc
Ye Zhang <ye.zhang2@magna.com>
2025-04-06 13:12:39 -0400

    add numbers.py

---
+++ numbers.py
@@ -0,0 +1,17 @@
+# this is a simple script to demonstrate the use of the numbers module
+# it will print the type of a number
+import numbers
+
+int a = 1
+int b = 2
+int c= 3
+int d = 4
+int e = 5
+int f = 6
+
+print("a is a number: ", isinstance(a, numbers.Number))
+print("b is a number: ", isinstance(b, numbers.Number))
+print("c is a number: ", isinstance(c, numbers.Number))
+print("d is a number: ", isinstance(d, numbers.Number))
+print("e is a number: ", isinstance(e, numbers.Number))
+print("f is a number: ", isinstance(f, numbers.Number))
