29e8e1cf090ae541891500b71eac68dde61410ca
Ye Zhang <ye.zhang2@magna.com>
2025-04-06 12:57:01 -0400

    Add "numbers.cpp"

---
+++ numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+
+    std::cout << "a + b + c = " << a + b + c << std::endl;
+    std::cout << "a * b * c = " << a * b * c << std::endl;
+    std::cout << "a - b - c = " << a - b - c << std::endl;
+    std::cout << "a / b / c = " << a / b / c << std::endl;
+}
